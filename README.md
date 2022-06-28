14770c49d941d40fd8d308387915cf2dc7ce7b26
serhiionyshchuk <sonyshchuk@deduce.today>
2022-06-28 12:00:29 +0300

    README file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-movie
- Responsive React Movies App With API
+For start project:
+1) git clone
+2) nmp i
+3) npm start
+4) In src/api/apiConfig.js (apiKey: 'get from themoviedb.org' -> your token)
+5) In file package.json ("start": "react-scripts start" -> "start": "react-scripts --openssl-legacy-provider start"
+   )
--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '017c2c32804753c758079ffa3dc983d9',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
